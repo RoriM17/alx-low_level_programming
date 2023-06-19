55794020477b0be1bfab7ec36dbfe9822bfa675e
Rorisang <Mmutlerorisang@gmail.com>
2023-06-19 09:57:49 -0700

    100-intel

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always O (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %lu byte(a)\n", sizeof(char));
+	printf("Size of an int: %lu byte(a)\n", sizeof(int));
+	printf("Size of a long int : %lu byte(a)\n", sizeof(long int));
+	printf("Size of a float : %lu byte(a)\n", sizeof(float));
+	return (0);
+}	
+~
